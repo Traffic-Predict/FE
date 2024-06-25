7205f5de9dfde34807e332ecd85d3de8623c3f61
Bh Jeon <bh@2p31.art>
2024-06-26 02:26:13 +0900

    Add: predict

--- map.js
+++ map.js
@@ -1,9 +1,25 @@
-var map = L.map('map').setView([36.332165597, 127.434310227], 13);  // 대전의 중심 좌표
+var map = L.map('map', {
+    zoomControl: false
+}).setView([36.332165597, 127.434310227], 13);  // 대전의 중심 좌표
+var mode = "";
+let modes = ["road", "predict"];
+map.addControl(L.control.zoom({ position: 'bottomright' }));
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
+
+function changeMode(modeName) {
+    if (modeName === mode) return;
+    mode = modeName;
+    for (let m of modes) {
+        for (let e of document.getElementsByClassName(m)) {
+            e.style.display = m === mode ? "block" : "none";
+        }
+    }
+    fetchTrafficData();
+}
 
 function clearMap() {
     for (i in map._layers) {
@@ -27,23 +43,28 @@ function fetchTrafficData() {
     let zoomLevel = map.getZoom();
     if (x0 > x1) [x0, x1] = [x1, x0];
     if (y0 > y1) [y0, y1] = [y1, y0];
-    //console.log(`Requesting traffic data for (${x0}, ${y0}) - (${x1}, ${y1})`);
+    url = 'https://carboncongestion.store/main';
+    query = {
+        minX: x0,
+        maxX: x1,
+        minY: y0,
+        maxY: y1,
+        zoom: zoomLevel
+    }
+    if (mode === "predict") {
+        query["time"] = document.getElementById("date").value + "T" + document.getElementById("time").value + ":00";
+        url = 'https://carboncongestion.store/predict/area';
+    }
     const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            minX: x0,
-            maxX: x1,
-            minY: y0,
-            maxY: y1,
-            zoom: zoomLevel
-        })
+        body: JSON.stringify(query)
     };
 
-    fetch('https://carboncongestion.store/main', requestOptions)
+
+    fetch(url, requestOptions)
         .then(response => response.json())
         .then(data => {
-            //console.log('Traffic data:', data);
             clearMap();
             displayTrafficData(data.items);
         })
@@ -77,5 +98,6 @@ map.on("load", fetchTrafficData);
 map.on("moveend", fetchTrafficData);
 
 window.onload = function () {
-    fetchTrafficData();
+    changeMode("road");
+
 }
